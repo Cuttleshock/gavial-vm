ab79cd2114c381a5ada49d6c1cd0d1d90247d8ce
Viktori Lavrentiev <48884484+Cuttleshock@users.noreply.github.com>
2023-03-23 17:41:15 +0000

    Define gvm_assert() (as yet unused)

--- src/common.c
+++ src/common.c
@@ -22,3 +22,17 @@ void gvm_error(const char *format, ...)
 	va_start(args, format);
 	log_to(stderr, format, args);
 }
+
+// If assertion is falsey, log a message or open a message box with the same contents
+// that assert_or_debug() would print; then exit the program cleanly.
+void gvm_assert(bool assertion, const char *format, ...)
+{
+	if (!assertion) {
+		va_list args;
+		va_start(args, format);
+		// TODO: Make text red (gvm_malloc(strlen(format) + 10-ish), strncat, log_to, gvm_free)
+		log_to(stderr, format, args);
+		// TODO: Cleaner exit
+		exit(EXIT_FAILURE);
+	}
+}
--- src/common.h
+++ src/common.h
@@ -5,7 +5,17 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#define RED "\x1B[31m"
+#define GRN "\x1B[32m"
+#define YLW "\x1B[33m"
+#define BLU "\x1B[34m"
+#define MAG "\x1B[35m"
+#define CYN "\x1B[36m"
+#define WHT "\x1B[37m"
+#define COLOR_RESET "\x1B[0m"
+
 void gvm_log(const char *format, ...);
 void gvm_error(const char *format, ...);
+void gvm_assert(bool assertion, const char *format, ...);
 
 #endif // COMMON_H
